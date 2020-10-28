7e1b40f9ddef4175f8b570f6140db256752e96c8
brad lumber <codedevbrad@gmail.com>
2020-10-28 20:44:24 +0000

    Update extension_function.js

--- _dev/dev/extension_function.js
+++ _dev/dev/extension_function.js
@@ -5,29 +5,29 @@ import { emotes_list as matched__emotes , emotes_html as emotes } from './_chatb
 import { template } from './_chatbox/_chatbox_template.js';
 import './extension_styles.scss';
 
-// var chatContainer = document.querySelector('.chat-scrollable-area__message-container');
-// const mutateString = ( words , string ) => {
-//     var newString = string;
-//     words.forEach( ( each_word ) => {
-//         newString = newString.replace( each_word[0] , `<img src=${ each_word[1] } width="32" height="32" />` );
-//     });
-//     return newString;
-// }
-//
-// var chat__watchMessages = new MutationObserver( ( e ) => {
-//
-//     let chatMsg__all  = chatContainer.querySelectorAll('.chat-line__message .text-fragment');
-//     let chatMsg__curr_index = chatMsg__all.length - 1;
-//     let chatMsg__curr = chatMsg__all[ chatMsg__curr_index ].innerHTML;
-//
-//     let mutated = mutateString( matched__emotes , chatMsg__curr );
-//
-//     let new__msg__container = chatContainer.querySelectorAll('.chat-line__message .text-fragment');
-//     let new__msg__ready = new__msg__container[ new__msg__container.length - 1 ];
-//
-//     new__msg__ready.innerHTML = mutated;
-// });
-// chat__watchMessages.observe( chatContainer , { childList: true });
+var chatContainer = document.querySelector('.chat-scrollable-area__message-container');
+const mutateString = ( words , string ) => {
+    var newString = string;
+    words.forEach( ( each_word ) => {
+        newString = newString.replace( each_word[0] , `<img src=${ each_word[1] } width="32" height="32" />` );
+    });
+    return newString;
+}
+
+var chat__watchMessages = new MutationObserver( ( e ) => {
+
+    let chatMsg__all  = chatContainer.querySelectorAll('.chat-line__message .text-fragment');
+    let chatMsg__curr_index = chatMsg__all.length - 1;
+    let chatMsg__curr = chatMsg__all[ chatMsg__curr_index ].innerHTML;
+
+    let mutated = mutateString( matched__emotes , chatMsg__curr );
+
+    let new__msg__container = chatContainer.querySelectorAll('.chat-line__message .text-fragment');
+    let new__msg__ready = new__msg__container[ new__msg__container.length - 1 ];
+
+    new__msg__ready.innerHTML = mutated;
+});
+chat__watchMessages.observe( chatContainer , { childList: true });
 
 
 class Chatbox extends HTMLElement {
